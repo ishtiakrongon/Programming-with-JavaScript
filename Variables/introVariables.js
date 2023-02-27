fc9ec994d7f8bbbb394a98f5752752104b1aec5a
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-02-27 19:08:19 +0600

    Create introVariables.js

---
+++ Variables/introVariables.js
@@ -0,0 +1,15 @@
+var person = "CyraxX";
+
+console.log("Hello, ", person);
+
+/* Arithmetic Operations*/
+
+console.log(1 + 15); // Summation
+console.log(52 - 62); // Subtraction
+console.log(85 / 3); // Dividation
+console.log(3 * 3); // Multiplication
+
+
+
+
+
