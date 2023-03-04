cca9b8eb2b9b842c80a10dfc25a99f57ea15a6e5
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-04 19:56:29 +0600

    update

---
+++ Variables/swtichCase.js
@@ -0,0 +1,20 @@
+// Declaring Variables
+
+var light = 'red';
+
+switch(light) {
+
+    case 'green':
+        console.log("Drive!");
+        break;
+    case 'orange':
+        console.log("Get Ready!");
+        break;
+    case 'red':
+        console.log("Stop!");
+        break;
+    default:
+        console.log("The light is not green, red or orange!");
+        break;
+
+}
