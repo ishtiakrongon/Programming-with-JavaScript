663546146b1a707fcf0f90e8fdd25ead43da3805
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-04 19:07:25 +0600

    if else statements

---
+++ Variables/ifElse.js
@@ -0,0 +1,21 @@
+// Declaring Variables
+
+var light = "green";
+
+if (light == "green") {
+
+    console.log("Drive the car");
+
+} else if (light == "orange") {
+
+    console.log("Get Ready!");
+
+} else if(light == "red") {
+
+    console.log("Stop the car!");
+
+} else {
+
+    console.log("Just keep Going!");
+
+}
