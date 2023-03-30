0780a472139a9768692d34602e86ae45ad59be1a
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-31 00:06:51 +0600

    updated

---
+++ loops/forOfloop.js
@@ -0,0 +1,7 @@
+//Arrays are iterable with for of loops
+
+const colors = ['Red', 'Orange', 'Yellow']
+
+for(var color of colors) {
+    console.log(color);
+}
---
+++ loops/keysandValues.js
@@ -0,0 +1,22 @@
+const car2 = {
+    speed: 200,
+    color: "red"
+}
+
+console.log(Object.keys(car2));
+
+// Values method
+
+const car3 = {
+    speed: 300,
+    color: "yellow"
+}
+
+console.log(Object.values(car3));
+
+//Object.entries() method will return both keys and values
+
+console.log(Object.entries(car3));
+
+//----------------------------------------------------------
+
