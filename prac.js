4beb1e2a80c29e697202baa2901a1b3fa3152e2a
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-30 20:32:28 +0600

    updated

--- prac.js
+++ prac.js
@@ -1,5 +1,13 @@
-try {
-    Number(5).toPrecision(300)
-    } catch(e) {
-    console.log("There was an error")
+// const arr = [1,2,3,4,5];
+// const newArr = arr.filter(num => num % 2 === 0);
+// console.log(newArr);
+
+const obj = {
+    prop1: {
+        prop2: {
+            prop3: 'value'
+        }
     }
+};
+
+console.log(obj.prop1.prop2.prop3.length);
