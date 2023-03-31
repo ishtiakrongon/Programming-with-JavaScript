5bdf4e41fe93bc10fc48041a8812581146ec1b06
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-03-31 15:12:27 +0600

    updated

---
+++ Basics/templateLiteral.js
@@ -0,0 +1,16 @@
+// ES6 multi-line template literals
+
+let multiLine = `
+    Using ES6
+    backticks,
+    multi-line
+    strings are
+    possible!
+`;
+
+console.log(multiLine);
+
+let firstName = `Md. Ishtiak Ahmed`;
+let lastName = `Rongon`;
+
+console.log(`${firstName} ${lastName}`);
