97a8704609609765cd35682f1666c7839fc9ba05
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-06-07 20:30:48 +0600

    update

--- ES6/tempCodeRunnerFile.js
+++ ES6/tempCodeRunnerFile.js
@@ -4,3 +4,17 @@ let myArray = ['Lycan','Tinker', 'Husker'];
 
 myArray.push('Akasha');
 console.log(myArray);
+
+// using pop() method to delete last element of the array.
+
+deleteLastElement = myArray.pop();
+console.log(deleteLastElement);
+
+// using shift method to show 1st element of the array.
+
+console.log(myArray.shift());
+
+// using unshift() method to add new element in the array in 1st position.
+
+newFirstElement = myArray.unshift('Phoenix');
+console.log(newFirstElement);
