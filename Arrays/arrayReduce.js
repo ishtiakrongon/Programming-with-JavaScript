6136b71a4dc8f4232f9832514a8df3367cc375f4
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-07-31 17:56:58 +0600

    added some practice code in array reduce file

--- Arrays/arrayReduce.js
+++ Arrays/arrayReduce.js
@@ -13,3 +13,38 @@ console.log(`Total price: $${total}`);
 //     return a + b;
 // }
 
+
+//--------------------------------------------------------------------------------------
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const sum = numbers.reduce(function (previousValue, currentValue) {
+//     return previousValue + currentValue;
+// })
+
+const sum = numbers.reduce( (acc, crr) => acc + crr );
+
+console.log(sum);
+
+// Using a for loop
+
+const sumFun = () => {
+    let acc = 0;
+    for (let curr of numbers) {
+        acc += curr;
+    }
+return acc;
+}
+console.log(sumFun());
+//-------------------------------------------------------------------------------------
+
+const cart = [
+    { id: 1, name: 'Product 1', price: 130 },
+    { id: 2, name: 'Product 2', price: 150 },
+    { id: 3, name: 'Product 3', price: 175 },
+]
+
+const totalCartPrice = cart.reduce( function (acc, product) {
+    return acc + product.price;
+},0 );
+console.log(totalCartPrice);
