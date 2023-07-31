84d012a2bb3e289d2d8efc7794b7277e78ce933a
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-07-31 12:42:07 +0600

    updated

--- Arrays/arrayFilter.js
+++ Arrays/arrayFilter.js
@@ -17,3 +17,27 @@ checkIncomeRange.forEach(print => console.log(print));
 
 //------------------------------------------------------------
 
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const evenNumbers = numbers.filter( number => number % 2 === 0 )
+
+evenNumbers.forEach(item => console.log(item));
+
+console.log("\n---------------------------------------------------------------\n");
+
+for (let evenNumber of evenNumbers) {
+    console.log(evenNumber);
+}
+
+console.log("\n---------------------------------------------------------------\n");
+
+for (let row = 0; row < evenNumbers.length; row++) {
+    console.log(evenNumbers[row]);
+}
+
+
+//--------------------------------------------------------------------------------------
+
+
+
+
