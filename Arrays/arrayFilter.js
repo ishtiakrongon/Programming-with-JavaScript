304c021400006c9f2c294d396334ab5c608ecc63
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-07-31 13:00:12 +0600

    array filter file updated

--- Arrays/arrayFilter.js
+++ Arrays/arrayFilter.js
@@ -38,6 +38,29 @@ for (let row = 0; row < evenNumbers.length; row++) {
 
 //--------------------------------------------------------------------------------------
 
+const companies = [
+    {name: 'Company One', category: 'Finance', start: 1981, end: 2004},
+    {name: 'Company two', category: 'Retail', start: 1992, end: 2008},
+    {name: 'Company three', category: 'Auto', start: 1999, end: 2007},
+    {name: 'Company four', category: 'Retail', start: 1989, end: 2010},
+    {name: 'Company five', category: 'Technology', start: 2009, end: 2014},
+    {name: 'Company six', category: 'Finance', start: 1987, end: 2010},
+    {name: 'Company seven', category: 'Auto', start: 1986, end: 1996},
+    {name: 'Company eight', category: 'Technology', start: 2011, end: 2016},
+    {name: 'Company nine', category: 'Retail', start: 1981, end: 1989}
+]
 
 
+// Get Only Retail Companies
 
+const retailCompanies = companies.filter( company => company.category === 'Retail' );
+
+console.log(retailCompanies);
+
+//-----------------------------------------------------------------------------
+
+// Get companies that started in or after 1980 and ended in or before 2005;
+
+const earlyCompanies = companies.filter(company => company.start >= 1980 && company.end <= 2005);
+
+console.log(earlyCompanies);
