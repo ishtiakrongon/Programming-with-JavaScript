e6f374bc4ed16193c1df5a3844129f87274632e5
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-07-31 18:17:55 +0600

    practice file added

---
+++ Arrays/practice.js
@@ -0,0 +1,37 @@
+const people = [
+    {
+        firstname: 'John',
+        lastname: 'Doe',
+        email: 'john@gmail.com',
+        phone: '111-111-1111',
+        age: 30
+    },
+    {
+        firstname: 'Jane',
+        lastname: 'Poe',
+        email: 'jane@gmail.com',
+        phone: '222-222-2222',
+        age: 45
+    },
+    {
+        firstname: 'Bob',
+        lastname: 'foe',
+        email: 'bob@gmail.com',
+        phone: '333-333-3333',
+        age: 45
+    },
+    {
+        firstname: 'Sara',
+        lastname: 'Soe',
+        email: 'sara@gmail.com',
+        phone: '444-444-4444',
+        age: 19
+    },
+    {
+        firstname: 'Jose',
+        lastname: 'Koe',
+        email: 'jose@gmail.com',
+        phone: '555-555-5555',
+        age: 23
+    },
+]
