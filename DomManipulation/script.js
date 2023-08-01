e47d4a3d77f2245b075a911697df68e12df691de
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-08-01 10:57:55 +0600

    new folder dom manipulation created

--- DomManipulation/script.js
+++ DomManipulation/script.js
@@ -1,2 +1,10 @@
-console.dir(window.document);
-console.log(document.body.innerText);
+// console.dir(window.document);
+// console.log(document.body.innerText);
+
+// console.log(document.links[0]);
+
+// document.write("Hello idiot!");
+
+console.log(document.getElementById('main'));
+const main = document.getElementById('main');
+main.innerHTML = '<h1>Hello from main</h1>';
