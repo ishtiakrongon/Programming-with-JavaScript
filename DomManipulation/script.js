5356e24e1b6dfa18672879c18939a9ed12b2ae5f
Md. Ishtiak Ahmed <ishtiak.rongon@gmail.com>
2023-08-02 11:18:51 +0600

    updated

--- DomManipulation/script.js
+++ DomManipulation/script.js
@@ -1,8 +1,43 @@
-let answer = prompt("What's your name?");
+// const body = document.body;
 
-if (typeof(answer) === 'string') {
-  let h1 = document.createElement('h1');
-  h1.innerText = answer;
-  document.body.innerText = '';
-  document.body.appendChild(h1);
-}
+// const div = document.createElement('div');
+
+// div.innerText = 'Hello World'
+
+// div.innerHTML = "<strong>It will now bold the text</strong>"
+
+// div.textContent = "Hello World 2"
+
+// body.append(div);
+
+// const div = document.querySelector('div');
+// console.log(div.textContent);
+// console.log(div.innerText);
+
+
+//---------------------------------------------------
+
+
+// const body = document.body;
+// const div = document.createElement('div');
+// body.append(div);
+
+// const strong = document.createElement('strong');
+// strong.innerText = "I don't know what I am doing";
+// div.append(strong);
+
+//--------------------------------------------------
+
+const body = document.body;
+const div = document.querySelector('div');
+const spanHi = document.querySelector('#hi');
+const spanBye = document.querySelector('#bye');
+
+
+spanBye.remove();
+div.append(spanBye);
+spanHi.setAttribute('class', 'text-change');
+spanHi.removeAttribute("title");
+spanBye.style.color = 'tomato';
+console.log(spanHi.title);
+
